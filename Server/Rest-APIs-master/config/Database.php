75df3d5ae904ead47179924b52ced47ee94d87d2
PAWAN MISHRA <pawone3@gmail.com>
2020-02-29 09:15:29 -0800

    Update Database.php

--- Server/Rest-APIs-master/config/Database.php
+++ Server/Rest-APIs-master/config/Database.php
@@ -3,10 +3,10 @@
 
 class Database {
     // DB params
-    private $host ='localhost';
+    private $host ='localhost:3306';
     private $db_name='ledger';
-    private $username = 'root';
-    private $password='';
+    private $username = 'sammy';
+    private $password='1@9!9458';
     private $conn;
 
     //DB connect
